76e80c1a8982c9f90e9584cbe6683f756a081697
z728359635 <728359635@qq.com>
2019-10-14 11:57:46 +0800

    work5

--- work5/work5.js
+++ work5/work5.js
@@ -1,8 +1,18 @@
 //9X9的空表，请自行编写这段代码
+str = '<table border="1">'
+for(let i=9;i>0;--i){
+    str += '<tr>'
+    for(var j=9;j>3;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table1').innerHTML = str
 
 
 //倒九九乘法空表
-str = '<table border="1">'
+str = '<table border="2">'
 for(let i=9;i>0;--i){
     str += '<tr>'
     for(var j=i;j>0;--j){
@@ -14,3 +24,13 @@ str += '</table>'
 document.getElementById('table2').innerHTML = str
 
 //倒九九乘法表 ， 请自行编写这段代码
+str = '<table border="3">'
+for (let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>' + j + '*' + i + '= ' + (j*i) + '</td>';
+    }
+    str +='</tr>'
+}
+str += '</table>'
+document.getElementById('table3').innerHTML = str
