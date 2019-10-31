e0c3b5f5421624679ea84b87eca0503388935399
728359635@qq.com <728359635@qq.com>
2019-10-31 16:38:58 +0800

    work10

--- work10/index.js
+++ work10/index.js
@@ -21,3 +21,14 @@ function monkey(){
     }
     document.getElementById('monkeyking').innerText = monkey[0]
 }
+function stat(){
+    let str =document.getElementById('str').value
+    let obj ={}
+    let shuzu=str.split("")
+    shuzu= shuzu.sort()
+    for (let i= 0; i< str.length; ++i){
+        let linshi= shuzu[i]
+        obj[linshi]= (obj [linshi] + 1) || 1;
+    } 
+    document.getElementById('result').innerText =JSON.stringify(obj)
+}
