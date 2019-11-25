f43e643a1613156357f5b944ae013329650d2a83
z728359635 <728359635@qq.com>
2019-11-25 13:54:22 +0800

    work12

--- work12/calendar.js
+++ work12/calendar.js
@@ -1,13 +1,13 @@
 function calendar(y){
-    var w = new Data(y,0).getDay();
+    var w = new Date(y,0).getDay();
 
     var html = '<div class="box">';
     for(var m = 1;m<= 12;++m){
         html +='<table>';
         html +='<tr class="title"><th colspan="7">'+ y + '年' + m + '月</th></tr>';
-        html +='<tr><td>日</td><td>-</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td><tr>';
+        html +='<tr><td>日</td><td>一</td><td>二</td><td>三</td><td>四</td><td>五</td><td>六</td><tr>';
 
-        var max = new Data(y,m,0).getData();
+        var max = new Date(y,m,0).getDate();
         html +='<tr>';
         for(var d= 1; d<=max; ++d){
             if(w && d == 1){
